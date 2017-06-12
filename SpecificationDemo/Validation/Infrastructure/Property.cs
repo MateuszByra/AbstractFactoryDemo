d8cb50f0c4289b9751b841646f607019a8be5de2
MateuszByra <mat.byra@gmail.com>
2017-06-12 20:13:25 +0200

    Property class with generic types of TType and TProperty.

--- SpecificationDemo/Validation/Infrastructure/Property.cs
+++ SpecificationDemo/Validation/Infrastructure/Property.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace SpecificationDemo.Validation.Infrastructure
 {
-    class Property
+    internal class Property<TType, TProperty> : Specification<TType>
     {
+        private Func<TType, TProperty> PropertyGetter { get; }
+        private Specification<TProperty> Subspecification { get; }
+
+        public Property(Func<TType, TProperty> propertyGetter,
+            Specification<TProperty> subspecification)
+        {
+            this.PropertyGetter = propertyGetter;
+            this.Subspecification = subspecification;
+        }
+
+        public override bool IsSatisfiedBy(TType obj) =>
+            this.Subspecification.IsSatisfiedBy(
+            this.PropertyGetter(obj));
     }
 }
