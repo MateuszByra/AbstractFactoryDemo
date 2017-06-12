fda84723ba5140b780249941b9dcf6eaf453edbf
MateuszByra <mat.byra@gmail.com>
2017-06-12 20:03:47 +0200

    NotNull Specification - NotNull class derived from Specification base class. Implement IsSatisfiedBy (check if obj equals null).

--- SpecificationDemo/Validation/Infrastructure/NotNull.cs
+++ SpecificationDemo/Validation/Infrastructure/NotNull.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SpecificationDemo.Validation.Infrastructure
 {
-    class NotNull
+    public class NotNull<T> : Specification<T>
     {
+        public override bool IsSatisfiedBy(T obj)=>
+            !object.ReferenceEquals(obj, null);
     }
 }
