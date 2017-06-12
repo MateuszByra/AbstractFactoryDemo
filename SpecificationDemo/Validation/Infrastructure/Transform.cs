f1cc931aa57bbd888711347da3887281e34babf7
MateuszByra <mat.byra@gmail.com>
2017-06-12 20:16:32 +0200

    Implement Transform generic class.

--- SpecificationDemo/Validation/Infrastructure/Transform.cs
+++ SpecificationDemo/Validation/Infrastructure/Transform.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace SpecificationDemo.Validation.Infrastructure
 {
-    class Transform
+    internal class Transform<T> : Specification<T>
     {
+        private Func<bool, bool> Transformation { get; }
+        private Specification<T> Subspecification { get; }
+
+        public Transform(Func<bool, bool> transformation,
+            Specification<T> specification)
+        {
+            this.Transformation = transformation;
+            this.Subspecification = specification;
+        }
+
+        public override bool IsSatisfiedBy(T obj) =>
+            this.Transformation(
+                this.Subspecification.IsSatisfiedBy(obj));
     }
 }
